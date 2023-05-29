5da9e33c5aa95cd994f08355a749beb814a3fbb7
sergiop <sergio.pinto@viewnext.com>
2023-05-29 22:41:36 +0200

    Modificado puerto

--- rhel9/main.tf
+++ rhel9/main.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "mi_grupo_de_seguridad" {
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
     description = "Acceso al puerto"
-    from_port   = 3000
-    to_port     = 3000
+    from_port   = 80
+    to_port     = 80
     protocol    = "TCP"
   }
 
