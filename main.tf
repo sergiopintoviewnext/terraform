3284f4d964a33f296a6de50508d52d2a0925a206
sergiop <sergio.pinto@viewnext.com>
2023-05-29 19:03:03 +0200

    Modificado main.tf con ingress port 22, acceso ssh, y egress a todo

--- main.tf
+++ main.tf
@@ -3,28 +3,46 @@ provider "aws" {
 }
 
 
-resource "aws_instance" "mi_segundo_servidor" {
-  ami             = "ami-0aef57767f5404a3c"
-  instance_type   = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hola Terraformers" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
+data "aws_vpc" "default" {
+  default = true
 }
 
+
+
+resource "aws_instance" "test_key" {
+  ami = "ami-008bcc0a51a849165"
+  instance_type = "t2.micro"
+  key_name      = "keys" //nombre clave ssh
+  vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
+
+}
+
+
 resource "aws_security_group" "mi_grupo_de_seguridad" {
-  name = "segundo_servidor_sg"
+  name   = "host_nodo_awx"
+
 
   ingress {
     cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso a puerto 8080 desde el exterior"
-    from_port = 8080
-    to_port = 8080
-    protocol = "TCP"
+    description = "Acceso al puerto "
+    from_port   = 22
+    to_port     = 22
+    protocol    = "TCP"
+  }
+
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Acceso al puerto "
+    from_port   = 80
+    to_port     = 80
+    protocol    = "TCP"
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }
 }
-
-
-
