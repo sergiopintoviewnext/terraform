dbe82f106d344a862e4b6b6d9e43a0d4d7e05420
sergiopintoviewnext <sergio.pinto@viewnext.com>
2023-06-26 17:22:50 +0200

    Actualizado en_red

--- en_red/main.tf
+++ en_red/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "instancia" {
 
   ami                    = each.value
   instance_type          = var.instance_type
-  key_name               = "keys" //nombre clave ssh
+  key_name               = "keys_work" //nombre clave ssh
   vpc_security_group_ids = [aws_security_group.mi_grupo_de_seguridad.id]
   subnet_id              = data.aws_subnet.az_a.id
 
@@ -37,38 +37,32 @@ resource "aws_security_group" "mi_grupo_de_seguridad" {
 
   name = "grupo_seguridad"
 
+  dynamic "ingress" {
+    for_each = var.list_ports
 
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso al puerto "
-    from_port   = 22
-    to_port     = 22
-    protocol    = "TCP"
+    content {
+      description = "Permitir trafico ${ingress.key}"
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = [var.cidr]
+    }
   }
-
-  ingress {
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Acceso al puerto"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "TCP"
-  }
-
 
   egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    description = "Permitir salida a internet"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [var.cidr]
   }
 
   ingress {
+    description = "Permitir ICMP"
     from_port   = -1
     to_port     = -1
     protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-    description = "Permitir ICMP"
+    cidr_blocks = [var.cidr]
   }
 
 }
