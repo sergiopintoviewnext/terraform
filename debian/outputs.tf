106abc1a36c97bf5629e048a6c44d211b263fe7e
sergiopintoviewnext <sergio.pinto@viewnext.com>
2023-06-26 17:47:47 +0200

    Actualizado debian

--- debian/outputs.tf
+++ debian/outputs.tf
@@ -1,7 +1,7 @@
-output "ipv4_publica" {
-  value = "La ipv4 pública de esta instancia es: ${aws_instance.debian.public_ip}"
+output "ip_publica" {
+  value = aws_instance.debian.public_ip
 }
 
 output "dns_publica" {
-  value = "La dns pública de esta instancia es: ${aws_instance.debian.public_dns}"
+  value = aws_instance.debian.public_dns
 }
